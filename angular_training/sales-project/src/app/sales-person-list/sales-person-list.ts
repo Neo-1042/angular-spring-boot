2614aaaa8b912440f5f697450dbb55cc65ec6d9a
Neo-1042 <rafaelh.71642@gmail.com>
2026-03-31 15:52:25 -0600

    First correct version of sales-person project
    ng generate class sales-person-list/SalesPerson

---
+++ angular_training/sales-project/src/app/sales-person-list/sales-person-list.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  standalone: false,
+  templateUrl: './sales-person-list.html',
+  styleUrl: './sales-person-list.css',
+})
+export class SalesPersonList implements OnInit {
+
+  // Create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@luv2code.com", 50000),
+    new SalesPerson("John", "Doe", "john.doe@luv2code.com", 40000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@luv2code.com", 90000),
+    new SalesPerson("Mai", "Troung", "mai.troung@luv2code.com", 60000),
+  ]
+
+  constructor() {}
+
+  ngOnInit() {}
+}
